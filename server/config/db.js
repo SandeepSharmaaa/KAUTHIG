623c118e40af083ea6b14cffd4575112aa0bbdb5
Sandeep Sharma <sandeepsharma18coding@gmail.com>
2026-09-02 00:06:30 +0530

    make that when i have entered the date it is coming in utc so i have made that datestrings =true so original date can be stored in utc

--- server/config/db.js
+++ server/config/db.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
     database: config.db.name,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0 ,
+    dateStrings:true
 });
 
 module.exports = pool;
